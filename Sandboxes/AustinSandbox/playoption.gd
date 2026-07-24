e379d372318aba697bfdd6ef488d0eb7cbc58f34
bbkkpluspplus <austinjgerardo@gmail.com>
2026-07-23 21:33:27 -0700

    replaced menu UI
    Replaced the main menu to 3D to enable slice to play

---
+++ Sandboxes/AustinSandbox/playoption.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var mesh = $ButtonMesh
+@onready var hitbox = $Hitbox
+@onready var text = $ButtonText
+
+var sliced := falseopti
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
