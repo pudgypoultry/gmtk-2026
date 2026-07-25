7b3ad9ddbb93788eda9ed426049de726749afb80
bbkkpluspplus <austinjgerardo@gmail.com>
2026-07-25 13:30:37 -0700

    Tutorial Dialogue
    Added tutorial dialogue

--- MainLevel/tutorial_level.gd
+++ MainLevel/tutorial_level.gd
@@ -1,4 +1,18 @@
 extends Node3D
+@onready var dialogue= $UI/Dialogue
+@onready var speaker=  $UI/Dialogue/Content/TextContainer/Speaker
+@onready var dialogue_text = $UI/Dialogue/Content/TextContainer/DialogueText
+@onready var continue_label= $UI/Dialogue/Content/TextContainer/Continue
+var dialogue_lines = [
+		"Ah good i was beginning to think 
+		you werent going to show up.",
+		" Lets begin before this headache kills me. 
+		Let your hand be the blade (MOVE YOUR MOUSE TO SLICE)",
+		" GOOD good! Now do you think your skills can cut 
+		down this bamboo instead of just AIR HAHA",
+		" Well... 
+		YOU STILL HAVE MUCH TO LEARN"]
+var current_line=0
 
 @onready var samurai: SamuraiCtrl = $Samurai
 @onready var samurai_master: SamuraiCtrl = $Samurai_Master
@@ -31,4 +45,27 @@ func move_camera(menu_active:bool) -> void:
 		loc = camera_loc_active_game.position
 	var tween = get_tree().create_tween()
 	tween.tween_property(camera_perspective, "position", loc, 1.0)
+	tween.finished.connect(show_dialogue)
 	#tween.tween_callback(### advance game function goes hear)
+	
+func show_dialogue():
+	current_line=0
+	dialogue.show()
+	speaker.text="Master:"
+	dialogue_text.text= dialogue_lines[current_line]
+	continue_label.text="Press [Space]"
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		next_dialogue()
+	
+func next_dialogue():
+	current_line +=1
+	if current_line>= dialogue_lines.size():
+		dialogue.hide()
+		return
+	dialogue_text.text = dialogue_lines[current_line]
+	
+	
+	
