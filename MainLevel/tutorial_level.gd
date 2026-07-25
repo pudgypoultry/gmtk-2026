5922656f9cc111aecaeead79ae80e492acffbda2
bbkkpluspplus <austinjgerardo@gmail.com>
2026-07-25 14:04:33 -0700

    Dialogue font change
    better dialogue appearance rolling script

--- MainLevel/tutorial_level.gd
+++ MainLevel/tutorial_level.gd
@@ -3,16 +3,21 @@ extends Node3D
 @onready var speaker=  $UI/Dialogue/Content/TextContainer/Speaker
 @onready var dialogue_text = $UI/Dialogue/Content/TextContainer/DialogueText
 @onready var continue_label= $UI/Dialogue/Content/TextContainer/Continue
-var dialogue_lines = [
+var dialogue_lines: Array[String]=[ 
 		"Ah good i was beginning to think 
 		you werent going to show up.",
-		" Lets begin before this headache kills me. 
-		Let your hand be the blade (MOVE YOUR MOUSE TO SLICE)",
+		"Begin before this headache kills me. 
+		Let your hand be the blade 
+		MOVE YOUR MOUSE TO SLICE",
 		" GOOD good! Now do you think your skills can cut 
 		down this bamboo instead of just AIR HAHA",
-		" Well... 
+		" Well, 
 		YOU STILL HAVE MUCH TO LEARN"]
 var current_line=0
+var dialogue_active: bool = false
+var is_typing: bool = false
+var typing_id: int=0
+
 
 @onready var samurai: SamuraiCtrl = $Samurai
 @onready var samurai_master: SamuraiCtrl = $Samurai_Master
@@ -22,6 +27,8 @@ var current_line=0
 
 @onready var main_menu_3d: Node3D = $"Camera-Perspective/MainMenu3D"
 @export var playbutton_slice_name:String = "Playoption"
+
+@export var typing_speed: float = 0.03
 
 
 func _ready() -> void:
@@ -48,24 +55,59 @@ func move_camera(menu_active:bool) -> void:
 	tween.finished.connect(show_dialogue)
 	#tween.tween_callback(### advance game function goes hear)
 	
-func show_dialogue():
+func show_dialogue() -> void:
+	dialogue_active= true
 	current_line=0
 	dialogue.show()
 	speaker.text="Master:"
-	dialogue_text.text= dialogue_lines[current_line]
+	
 	continue_label.text="Press [Space] to continue"
+	type_text(dialogue_lines[current_line])
 	
 	
 func _unhandled_input(event):
-	if event.is_action_pressed("ui_accept"):
+	if dialogue_active and event.is_action_pressed("ui_accept"):
 		next_dialogue()
+		get_viewport().set_input_as_handled()
 	
 func next_dialogue():
+	if is_typing:
+		finish_current_line()
+		return
 	current_line +=1
 	if current_line>= dialogue_lines.size():
-		dialogue.hide()
+		end_dialogue()
 		return
-	dialogue_text.text = dialogue_lines[current_line]
+	type_text(dialogue_lines[current_line])
+	
+func type_text(text_to_show: String) -> void:
+		typing_id +=1
+		var current_typing_id := typing_id
+		is_typing=true
+		dialogue_text.text=""
+		
+		for letter in text_to_show:
+			if current_typing_id != typing_id:
+				return
+			dialogue_text.text += letter
+			await get_tree().create_timer(typing_speed).timeout
+		if current_typing_id == typing_id:
+			is_typing = false
+			
+func finish_current_line() -> void:
+	typing_id +=1
+	is_typing= false
+	
+	dialogue_text.text= dialogue_lines[current_line]
+	
+func end_dialogue() -> void:
+	typing_id =+1
+	is_typing=false
+	dialogue_active= false
+	dialogue.hide()
+				
+					
+
 	
 	
 	
